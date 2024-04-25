2bc568e18573701f0cda9abf83ee4f40583fe2e0
Elise <21520069@gm.uit.edu.vn>
2024-04-25 23:24:45 +0700

    add entities, define some global paths

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Elearning.Entities;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Elearning
 {
     internal static class Program
     {
+        // Paths to folders in the project
+        public static string AVARTAR_PATH = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..")) 
+                                            + "\\images\\avatars\\";
+        public static string RESOURCES_PATH = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..")) 
+                                            + "\\resources\\";
+        public static string COURSES_IMG_PATH = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..")) 
+                                            + "\\images\\courses_images\\";
+
+        // Create a new instance of the ELEARNINGEntities, use this instance to interact with the database
+        // example: var accounts = Program.provider.ACCOUNTs.ToList();
+        public static ELEARNINGEntities provider = new ELEARNINGEntities();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
